4016dfc7c5e4ae0fa50b0cb7007f4f8e745562ca
Nitesh <nitesh@acer.home.local>
2026-01-22 00:02:58 +0530

    chore: Day-14 of 250 days challenge::completed

---
+++ JS/DATA_TYPES/index.js
@@ -0,0 +1,60 @@
+// Data types in Javascript
+
+/*
+
+* there are  mainly two types of data types-
+1. primitive datatypes
+2. non-primitive datatypes (reference types)
+*/
+
+// string
+console.log("Hello World")
+console.log('Hey,')
+console.log(`Hello "World"`)
+
+// number
+console.log(34)
+console.log(-34)
+console.log(3.4)
+console.log(34.565)
+
+
+// boolean
+console.log(true)
+console.log(false)
+
+// null
+console.log(null)
+
+// undefined
+console.log(undefined)
+
+/**
+ * Conversion of string to number datatype
+ */
+
+// first way
+console.log(+"43.56")
+console.log(+"-43.56")
+console.log(+"43")
+console.log(+"43bg")
+
+// second way
+console.log(Number("hmm"))
+console.log(Number("100"))
+console.log(Number("100.1"))
+console.log(Number("-100"))
+
+// third way
+console.log(parseInt("Hey, there"))
+console.log(parseInt(45))
+console.log(parseInt(-45))
+console.log(parseInt(-45.5))
+
+// fourth way
+console.log(parseFloat("454"))
+console.log(parseFloat("-454"))
+console.log(parseFloat("454fg"))
+console.log(parseFloat("-4.54"))
+
+
