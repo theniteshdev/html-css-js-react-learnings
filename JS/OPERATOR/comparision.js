a571d25ec76b9678c4fcfbd19868113066679714
Nitesh <nitesh@acer.home.local>
2026-01-24 23:16:02 +0530

    day-17/250 JS- comparision operator::completed

---
+++ JS/OPERATOR/comparision.js
@@ -0,0 +1,35 @@
+// COMPARISION OPERATOR
+/**
+ * =        |-> ASSIGNMENT OPR
+ * ==       |-> EQUAL TO
+ * ===      |-> EQUAL TO AND EQUAL TYPE
+ * !==      |-> NOT EQUAL
+ * !===     |-> NOT EQUAL TO AND NOT EQUAL TYPE
+ * >        |-> GREATER THAN
+ * <        |-> LESS THAN
+ * >=       |-> GREATER THAN OR EQUAL TO
+ * <=       |-> LESS THAN OR EQUAL TO 
+ */
+
+let test1 = "hello"
+let test2 = "world"
+let t1 = "hey"
+let t2 = "hey"
+let x = 34
+let y = 45
+
+test1 == test2
+test1 === test2
+
+t1 == t2
+t1 === t2
+
+test1 != "apple"
+test1 !== test2
+
+x > y
+y > x
+y >= y
+y <= x
+
+// Today I learn Comparison Operator in JavaScript - day-17 / 250 days
