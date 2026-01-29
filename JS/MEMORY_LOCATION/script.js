744f40a8df0b3910a0540b5b4e2c23e865d133d9
Nitesh <nitesh@acer.home.local>
2026-01-29 23:00:14 +0530

    day-21/250days js- memory-tab in depth to understood::completed

---
+++ JS/MEMORY_LOCATION/script.js
@@ -0,0 +1,20 @@
+const firstname = "nitesh";
+const age = 15;
+const isGradute = false;
+
+const hasJob = false; // their memory locatio is-@141
+
+let job = hasJob; // their memory location is same-@141
+
+
+let firstname2 = firstname; // here value is "nitesh" and their location is same but how ?
+
+// the location of firstname and firstname2 has same - how
+
+// in the memory tab their memory address is same
+
+
+const u1 = "hello.world"; // here also their memory address is same- @117
+const u2 = "hello.world"; // memory address is same- @117
+
+// here the same value but for different var so it create a single memory location provide to both
