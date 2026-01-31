e4d0d15d303855d6b66449077a2cc3c415859ec1
Nitesh <nitesh@acer.home.local>
2026-01-31 21:17:25 +0530

    day-22/250 days JS-objects + innerworkings ::completed

---
+++ JS/OBJECTS/object.pros.js
@@ -0,0 +1,6 @@
+/* what i have learned
+-> objects inner working
+-> freeze and seal keyword 
+-> in keyword
+-> isFrozen keyword
+*/
--- JS/OBJECTS/script.js
+++ JS/OBJECTS/script.js
@@ -33,3 +33,23 @@ console.log(a.firstname == b.firstname); // this is true
 console.log(a.firstname === b.firstname); // this is true
 
 console.log(a["firstname"]);// behind the scence keys are stored like string
+
+// creating nesting objects
+const loginDets = {
+    username: "nitesh",
+    password: "nitesh@123",
+    email: "niteshkumar@gmail.com",
+    lastloginDetails: {
+        date: 12,
+        month: 2,
+        year: 2026,
+        time: {
+            hour: 23,
+            seonds: 55,
+            minutes: 3
+        }
+    }
+};
+
+Object.seal(loginDets)
+// Object.freeze(loginDets)
