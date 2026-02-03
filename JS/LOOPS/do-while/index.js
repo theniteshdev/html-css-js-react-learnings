60cac4b659196992e4ff8c33455f711b2644a293
Nitesh <nitesh@acer.home.local>
2026-02-03 22:56:21 +0530

    day-26/250 days JS: for-loop, do-while-loop::complete

---
+++ JS/LOOPS/do-while/index.js
@@ -0,0 +1,12 @@
+// Do While 
+
+/*run once in any condition*/
+let i = 1;
+do {
+    console.log(`${i}.Hey`)
+    ++i
+} while (i <= 20);
+
+do {
+    console.log("Hmm"); // run once
+} while (false); // this is false but always run once
---
+++ JS/LOOPS/for/index.js
@@ -0,0 +1,14 @@
+// Learing For Loops in JS
+for (let i = 1; i <= 10; i++) {
+    // console.log(i)
+};
+// console.log(i); // error
+
+// printing all odd numbers between 1 to 20
+for (var i = 1; i <= 20; ++i) {
+    if (!(i % 2 === 0)) {
+        console.log(i)
+    }
+};
+console.log(i); // no errors
+// var is global variable so its log
