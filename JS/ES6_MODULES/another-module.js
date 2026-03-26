4ea1f71a55e4b11ab524e2f495d63bb4582bb65a
theniteshdev <0911nitesh@gmail.com>
2026-03-26 22:12:29 +0530

    learn es6 modules in javascript and overview of package.json file

---
+++ JS/ES6_MODULES/another-module.js
@@ -0,0 +1,7 @@
+// this is another-module
+import addFunction, { name, } from "./index.js"; // file extension is required
+console.log(name);
+// console.log(username);
+
+console.log(addFunction(1, 3))
+console.log(addFunction(1, 44))
---
+++ JS/ES6_MODULES/index.js
@@ -0,0 +1,6 @@
+// this is main file
+const username = "nitesh.dev";
+export default function (a, b) {
+    return a + b;
+};
+export { username as name };
