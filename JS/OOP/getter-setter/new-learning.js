86a23d1c744895ff6a36e8721dc98ffd52c1812c
theniteshdev <0911nitesh@gmail.com>
2026-03-28 15:01:55 +0530

    find the new learning with destructuring

--- JS/OOP/getter-setter/new-learning.js
+++ JS/OOP/getter-setter/new-learning.js
@@ -7,7 +7,35 @@ class Laptop {
     }
     set setRam(newRam) {
         this.ram = newRam;
-    };
+
+        "use strict";
+
+        function createCounter() {
+            let count = 0;
+
+            return {
+                increment() { count++; },
+                getCount() { return count; },
+                reset: function () { count = 0; }
+            };
+        }
+
+        const counter = createCounter();
+        counter.increment();
+        counter.increment();
+        counter.increment();
+
+        const { getCount, reset } = counter;
+
+        try {
+            reset();
+            console.log("After reset:", counter.getCount());
+        } catch (e) {
+            console.log("Error:", e.message);
+        }
+
+        console.log("Direct call:", counter.getCount());
+         };
 
     get getRam() {
         return this.ram;
