167cda384339c2f6a7c6c0dec5227e1978d20447
theniteshdev <0911nitesh@gmail.com>
2026-03-28 11:18:59 +0530

    getter and setter in js

---
+++ JS/OOP/getter-setter/getter-setter.js
@@ -0,0 +1,32 @@
+// getter and setter
+
+class Laptop{
+ram;
+rom;
+constructor(ram, rom){
+    this.ram = ram;
+    this.rom = rom;
+}
+
+get getLaptopRam(){
+    return this.ram
+};
+
+set setLaptopRam(ram){
+    this.ram = ram;
+    console.log(this.ram)
+    return this.ram;
+}
+
+};
+
+const l1 = new Laptop('16gb', '128gb');
+
+console.log(l1);
+console.log(l1.getLaptopRam);
+l1.setLaptopRam = '8gb';
+
+/**
+set -> its a function but not call just access like property.
+get -> its a function but call like property and provide the return value
+ */
