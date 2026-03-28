2fb526638dc6bfd7f3222996a85e4427dbab2d00
theniteshdev <0911nitesh@gmail.com>
2026-03-28 11:32:27 +0530

    getter-setter.js done with new learning

--- JS/OOP/getter-setter/getter-setter.js
+++ JS/OOP/getter-setter/getter-setter.js
@@ -1,22 +1,22 @@
 // getter and setter
 
-class Laptop{
-ram;
-rom;
-constructor(ram, rom){
-    this.ram = ram;
-    this.rom = rom;
-}
+class Laptop {
+    ram;
+    rom;
+    constructor(ram, rom) {
+        this.ram = ram;
+        this.rom = rom;
+    }
 
-get getLaptopRam(){
-    return this.ram
-};
+    get getLaptopRam() {
+        return this.ram
+    };
 
-set setLaptopRam(ram){
-    this.ram = ram;
-    console.log(this.ram)
-    return this.ram;
-}
+    set setLaptopRam(ram) {
+        this.ram = ram;
+        console.log(this.ram)
+        return this.ram;
+    }
 
 };
 
@@ -30,3 +30,14 @@ l1.setLaptopRam = '8gb';
 set -> its a function but not call just access like property.
 get -> its a function but call like property and provide the return value
  */
+
+let { getLaptopRam, setLaptopRam, ram, rom } = l1; // destructuing the fields
+
+console.log(getLaptopRam)
+setLaptopRam = 'testtt'; // here this is not working
+console.log('getting laptop\'s ra')
+console.log(getLaptopRam)
+
+console.log(ram)
+console.log(rom)
+
